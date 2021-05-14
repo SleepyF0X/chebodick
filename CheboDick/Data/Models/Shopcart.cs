89957b489e170a8500ccc5f36c42d2ec1acc6e35
zefirlover <ibodikmushta@gmail.com>
2021-05-14 23:18:08 +0300

    CD- EF Core Some basic models and AppDBContext were created. Models/Shopcart.cs has no logic and in AppDBContext it commented, yet

---
+++ CheboDick/Data/Models/Shopcart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheboDick.Data.Models
+{
+    public class Shopcart
+    {
+        public int Id { get; set; }
+    }
+}
