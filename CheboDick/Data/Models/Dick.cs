c5fb682edf8f19ad57549fa12976fae9d95d617a
zefirlover <ibodikmushta@gmail.com>
2021-05-15 18:16:38 +0300

    CD-1 EF Core Models and AppDBContext added

---
+++ CheboDick/Data/Models/Dick.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheboDick.Data.Models
+{
+    public class Dick
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
